cb0c5d9bb32fe54cfa328e07a75a78abfbd9dac1
Kgotso Bapela <kgotso.bapela@gmail.com>
2023-08-26 19:59:03 +0200

    Project completed

---
+++ script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll('.toggle'),
+      good = document.querySelector('#good'),
+      cheap = document.querySelector('#cheap'),
+      fast = document.querySelector('#fast');
+
+
+toggles.forEach(toggles => toggles.addEventListener('change', (e) => doTheTrick(e.target)));
+
+function doTheTrick(theClickedOne){
+    if(good.checked && cheap.checked && fast.checked){
+        if(good === theClickedOne){
+            fast.checked = false;
+        }
+        if(cheap === theClickedOne){
+            good.checked = false;
+        }
+        if(fast === theClickedOne){
+            cheap.checked = false;
+        }
+    }
+}
+
